496e45ce4670e27af9e41e52968cc421f1f82d31
“mayluv17” <mayowaibrahim009@gmail.com>
2023-07-17 15:43:20 +0100

    created socket connection and draw

--- app/page.tsx
+++ app/page.tsx
@@ -9,24 +9,7 @@ const page: FC<pageProps> = ({}) => {
   const [color, setColor] = useState<string>("#000");
   const { canvasRef, onMouseDown, clear } = useDraw(drawLine);
 
-  function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
-    const { x: currX, y: currY } = currentPoint;
-    const lineColor = color;
-    const lineWidth = 5;
-
-    let startPoint = prevPoint ?? currentPoint;
-    ctx.beginPath();
-    ctx.lineWidth = lineWidth;
-    ctx.strokeStyle = lineColor;
-    ctx.moveTo(startPoint.x, startPoint.y);
-    ctx.lineTo(currX, currY);
-    ctx.stroke();
-
-    ctx.fillStyle = lineColor;
-    ctx.beginPath();
-    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
-    ctx.fill();
-  }
+  function drawLine({ prevPoint, currentPoint, ctx }: Draw) {}
 
   return (
     <>
